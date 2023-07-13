3156983fb7d3e53d539718996a4aee9ad3951ce7
Hora <horaciob@gmail.com>
2023-07-13 12:57:04 +0200

    change requires

--- some.rb
+++ some.rb
@@ -1,13 +1,19 @@
 require 'bundler/setup'
-require_relative './custom/types/custom_regex'
-
 Bundler.require
+require_all './custom/types'
 
 registry_url = 'http://localhost:8081'
 
 Avromatic.configure do |avro|
   avro.schema_store = AvroTurf::SchemaStore.new(path: './contracts')
   avro.registry_url = registry_url
+
+  avro.register_type('handshake') do |type|
+    type.from_avro = ->(value) { value.downcase }
+    type.to_avro = ->(value) { value.downcase }
+  end
   
   avro.build_messaging!
 end
+
+require_all 'models'
