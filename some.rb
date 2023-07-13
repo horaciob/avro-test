b519f7e3f2c13f5d5edf6b24fd83bbed05d32a7b
Hora <horaciob@gmail.com>
2023-07-13 11:43:52 +0200

    contract

--- some.rb
+++ some.rb
@@ -8,15 +8,6 @@ registry_url = 'http://localhost:8081'
 Avromatic.configure do |avro|
   avro.schema_store = AvroTurf::SchemaStore.new(path: './contracts')
   avro.registry_url = registry_url
-
-  avro.register_type('foobar', String) do |type|
-    type.from_avro = ->(value) do 
-      raise "oh no!"
-      puts "value"
-      value.upcase 
-    end
-    type.to_avro = ->(value) { value.downcase }
-  end
   
   avro.build_messaging!
 end
